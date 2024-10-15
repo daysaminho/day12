91037dfb03a162753da7d1a8b09e65809df10059
daysaminho <daysam.moussaoui@epitech.digital>
2024-10-15 13:45:07 +0200

    ex_01/

---
+++ ex_01/ex_01.js
@@ -0,0 +1,6 @@
+async function display_weather_chart(longitude, latitude){
+    const url = "https://www.7timer.info/bin/civil.php?lon="+longitude+"&lat="+latitude+"&product=civil&output=internal&unit=metric&tzshift=0"
+
+}
+
+display_weather_chart(2.367 , 48.817)
